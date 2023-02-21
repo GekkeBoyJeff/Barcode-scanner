9de77e6420f556126744b0f9fd4fa00a4c9adcab
GekkeBoyJeff <jeffrey.ullers@hva.nl>
2023-02-21 02:02:05 +0100

    Added a loading state for the video

--- js/modules/handleCamera.js
+++ js/modules/handleCamera.js
@@ -1,9 +1,15 @@
 import { detectCode } from './detectBarcode.js';
 
 export async function enableCamera() {
+
+    // Flag to indicate if the function is currently loading
+    let isLoading = true;
+    document.querySelector('section:nth-of-type(2) > video').classList.toggle('skeleton')
+
     // Check if device has camera
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-        // Use video without audio
+
+        // Define constraints to use the camera without audio and with the back camera
         const constraints = {
             audio: false,
             video: {
@@ -12,26 +18,33 @@ export async function enableCamera() {
         }
 
         try {
+            // Request access to the user's camera using the defined constraints
             const stream = await navigator.mediaDevices.getUserMedia(constraints);
+
+            // Play the camera stream
             video.srcObject = stream;
             video.play();
 
-
             // https://developer.mozilla.org/en-US/docs/Web/API/MediaStreamTrack/getSettings
             // https://developer.mozilla.org/en-US/docs/Web/API/MediaStreamTrack/getCapabilities
+            // Get camera track and capabilities
             const track = stream.getVideoTracks()[0];
             const capabilities = track.getCapabilities();
 
+            // Check if focus distance is supported or not
             if (!capabilities.focusMode) {
                 return;
             }
 
+            // Check if focus distance setting is supported or not
+            // https://developer.mozilla.org/en-US/docs/Web/API/MediaTrackCapabilities/focusDistance
             const input = document.querySelector('input[type="range"]');
             input.min = capabilities.focusDistance.min;
             input.max = capabilities.focusDistance.max;
             input.step = capabilities.focusDistance.step;
             input.value = track.getSettings().focusDistance;
 
+            // Add event listener to update focus distance
             if (!input.classList.contains('hidden')) {
                 input.oninput = function (event) {
                     // https://developer.mozilla.org/en-US/docs/Web/API/MediaStreamTrack/applyConstraints
@@ -49,6 +62,16 @@ export async function enableCamera() {
         catch (error) {
             console.log(error);
         }
+        finally {
+            // Set loading flag to false
+            isLoading = false;
+        }
+    } else {
+        console.log("getUserMedia is not supported");
+    }
+    if (!isLoading) {
+        document.querySelector('section:nth-of-type(2) > video').classList.toggle('skeleton')
+        console.log('stop loading')
     }
 }
 
